5e5fee7f1e0b7042a91ebd6695f7b01d0ab1b398
Filipe Brandao <fdabrandao@gmail.com>
2018-05-18 14:48:18 +0100

    Prepare release 2.0.0.0-080518

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ copyright = u'2013-2018, AMPL Inc'
 # The short X.Y version.
 version =  '1.4'
 # The full version, including alpha/beta/rc tags.
-release = '1.4.0.0-250218'
+release = '2.0.0.0-080518'
 
 
 
