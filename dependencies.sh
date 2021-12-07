85bc60762ac72167b9c31f38de0849bf8a19e1b0
Filipe Brandão <fdabrandao@gmail.com>
2021-12-07 00:08:32 +0000

    Fix installation of testthat on Windows

--- dependencies.sh
+++ dependencies.sh
@@ -3,4 +3,4 @@ BASEDIR=`dirname $0`
 cd $BASEDIR
 set -ex
 
-R --no-save -q -e "install.packages(c('Rcpp', 'testthat'), repos='http://cran.us.r-project.org')"
+R --no-save -q -e "install.packages(c('Rcpp', 'testthat'), repos='http://cran.us.r-project.org', INSTALL_opts=c('--no-multiarch'))"
