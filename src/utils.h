0c98377175f69e7fdb8947c41d13d0b599bad025
Jurgen Lentz <jurgenlentz26@gmail.com>
2024-10-12 14:34:14 +0200

    fix bug

--- src/utils.h
+++ src/utils.h
@@ -10,7 +10,7 @@ Rcpp::XPtr<T> getObj(const std::string &type, SEXP s) {
   std::string rtypename = "Rcpp_" + type;
   Rcpp::S4 s4obj(s);
   if ( !s4obj.is( rtypename.c_str() ) ) {
-    Rf_error( (std::string("object is not of the type ")+type).c_str() );
+    Rf_error("%s", (std::string("object is not of the type ")+type).c_str());
   }
   Rcpp::Environment env(s4obj);
   Rcpp::XPtr<T> xptr(env.get(".pointer"));
